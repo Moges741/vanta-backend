c8794acfdc053931341d52dc83f8b2fb56f8502f
Moges741 <mogesse741@gmail.com>
2026-03-31 00:19:41 +0300

    added auth feature

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
   app.enableCors();
 
   app.useGlobalPipes(
