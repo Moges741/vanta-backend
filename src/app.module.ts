a7f73da75fe36d045aca16414872b021ecddf327
Moges741 <mogesse741@gmail.com>
2026-03-30 23:25:04 +0300

    added users service, and dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+// import { UsersController } from './users/users.controller';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+// import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      envFilePath: '.env',
+    }),
+    PrismaModule,
+    UsersModule,
+    // AuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
