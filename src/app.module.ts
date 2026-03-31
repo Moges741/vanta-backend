1472c9947bcaa8fb3f60ceafe6ae3afe8b59b691
Shambel Dechu <shambeldechu@gmail.com>
2026-03-31 21:21:34 +0300

    chore: comment the duplicate file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+//import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EventsModule } from './events/events.module';
-import { EventImagesModule } from './event-images/event-images.module';
-import { EventStepsModule } from './event-steps/event-steps.module';
+import { EventImagesModule } from './events/event-images/event-images.module';
+import { EventStepsModule } from './events/event-steps/event-steps.module';
 import { UploadsModule } from './uploads/uploads.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [EventsModule, EventImagesModule, EventStepsModule, UploadsModule, AuthModule],
-  controllers: [AppController],
+  //controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
