4e586d5b544d0dbb5bdcd566323a7a5b1fccdd85
Shambel Dechu <shambeldechu@gmail.com>
2026-03-31 21:13:25 +0300

    feat: add steps controller, module, and service

---
+++ src/events/event-steps/event-steps.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { EventStepsController } from './event-steps.controller';
+import { EventStepsService } from './event-steps.service';
+import { PrismaService } from '../../../prisma/prisma.service';
+@Module({
+  imports: [],
+  controllers: [EventStepsController],
+  providers: [EventStepsService, PrismaService]
+})
+export class EventStepsModule {}
+
