eab56d4f808182b7513d38da23567371d78ff3cf
Shambel Dechu <shambeldechu@gmail.com>
2026-04-06 12:07:47 +0300

    fix: import prisma path

--- src/events/event-steps/event-steps.module.ts
+++ src/events/event-steps/event-steps.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { EventStepsController } from './event-steps.controller';
 import { EventStepsService } from './event-steps.service';
-import { PrismaService } from '../../../prismar/prisma.service';
 @Module({
   imports: [],
   controllers: [EventStepsController],
-  providers: [EventStepsService, PrismaService]
+  providers: [EventStepsService]
 })
 export class EventStepsModule {}
 
